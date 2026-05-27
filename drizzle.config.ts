e954c60f662ef7a57e2243e520e727bd07ae50bb
Joaquin Mendez <joaquin.enmt@gmail.com>
2026-05-27 17:44:17 -0400

    Test: creando bd desde el inicio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="bun"/>
-import { defineConfig } from "drizzle-kit";
+import { defineConfig, type Config } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
@@ -8,4 +8,4 @@ export default defineConfig({
   dbCredentials: {
     url: `:memory:`,
   },
-});
+}) satisfies Config;
--- shared/drizzle/db.ts
+++ shared/drizzle/db.ts
@@ -4,6 +4,14 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 const sqlite = new Database(":memory:");
 
+sqlite.run(`CREATE TABLE registros (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	rut text NOT NULL,
+	nombres text,
+	apellidos text,
+	tipo text,
+	marcacion text`)
+
 export const db = drizzle({ client: sqlite });
 
 migrate(db, { migrationsFolder: "./drizzle" });
