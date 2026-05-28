4ab27e38d12574547e0924220c72e70676b76b1e
Joaquin Mendez <joaquin.enmt@gmail.com>
2026-05-28 12:41:39 -0400

    test: fix libreria libsql

--- server/api/ver.get.ts
+++ server/api/ver.get.ts
@@ -2,7 +2,7 @@ import { db } from "#shared/drizzle/db";
 import { registrosTable } from "#shared/drizzle/schema";
 
 export default eventHandler(async () => {
-  //const result = db.query("select * from REGISTROS;").all();
+
   const result = await db.select().from(registrosTable);
   return result;
 });
--- shared/drizzle/db.ts
+++ shared/drizzle/db.ts
@@ -1,12 +1,9 @@
-//import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client"
-//import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import { createClient } from "@libsql/client/web"
 
-//const sqlite = new Database(":memory:");
 const client = createClient({
 	authToken: Bun.env.TURSO_AUTH_TOKEN!,
 	url: Bun.env.TURSO_DB_URL!,
 })
 
-export const db = drizzle({ client });
+export const db = drizzle(client);
