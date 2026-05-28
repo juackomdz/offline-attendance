85e36e02e6e9eb3bb068684e38d7c37e99ea313f
Joaquin Mendez <joaquin.enmt@gmail.com>
2026-05-28 12:12:30 -0400

    test: cambia a turso como db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,9 @@ import { defineConfig, type Config } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   schema: "./shared/drizzle/schema.ts",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
-    url: `:memory:`,
+    url: Bun.env.TURSO_DB_URL!,
+    authToken: Bun.env.TURSO_AUTH_TOKEN!,
   },
 }) satisfies Config;
--- shared/drizzle/db.ts
+++ shared/drizzle/db.ts
@@ -1,17 +1,12 @@
-import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+//import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client"
+//import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const sqlite = new Database(":memory:");
+//const sqlite = new Database(":memory:");
+const client = createClient({
+	authToken: Bun.env.TURSO_AUTH_TOKEN!,
+	url: Bun.env.TURSO_DB_URL!,
+})
 
-sqlite.run(`CREATE TABLE registros (
-	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	rut text NOT NULL,
-	nombres text,
-	apellidos text,
-	tipo text,
-	marcacion text)`)
-
-export const db = drizzle({ client: sqlite });
-
-migrate(db, { migrationsFolder: "./drizzle" });
+export const db = drizzle({ client });
