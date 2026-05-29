a6729216877c0541235c4062a04fcc74ca8d7d38
Joaquin Mendez <joaquin.enmt@gmail.com>
2026-05-29 10:46:27 -0400

    modificada conexion db

--- shared/drizzle/db.ts
+++ shared/drizzle/db.ts
@@ -1,12 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql/web";
-//import { createClient } from "@libsql/client/web"
 
-/*
-const client = createClient({
-	authToken: process.env.TURSO_AUTH_TOKEN!,
-	url: process.env.TURSO_DB_URL!,
-})
-*/
 export const db = drizzle({
 	connection: {
 		url: process.env.TURSO_DB_URL!,
